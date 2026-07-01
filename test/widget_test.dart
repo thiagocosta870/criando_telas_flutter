0b850775f9abe14afda9df92aa07ed1f184cffd1
thiagocosta870 <titocosta416@gmail.com>
2026-07-01 12:43:27 -0300

    Correções

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:telas_atividade/main.dart';
+import 'package:telas_atividade/tela1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
